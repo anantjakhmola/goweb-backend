b643ec434f0e8e0ee74b02f461f12c0390109b23
anant jakhmola <anantjakhmola9@mail.com>
2021-02-23 02:36:10 +0530

    simplifying view logic

--- main.go
+++ main.go
@@ -15,20 +15,14 @@ var contactView *views.View
 func home(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type,", "text/html")
 	//fmt.Fprint(w, "<h1>Welcomne to my awesome site</h1>")
-	err := homeView.Template.ExecuteTemplate(w, homeView.Layout, nil)
-	if err != nil {
-		panic(err)
-	}
+	must(homeView.Render(w, nil))
 
 }
 
 func contact(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type,", "text/html")
 	//fmt.Fprintf(w, "To get in touch please send email to <a href=\"mailto:anantjakhmola9@gmail.com\">support@anantmail.com</a>")
-	err := contactView.Template.ExecuteTemplate(w, contactView.Layout, nil)
-	if err != nil {
-		panic(err)
-	}
+	must(contactView.Render(w, nil))
 }
 func faq(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type,", "text/html")
@@ -44,4 +38,10 @@ func main() {
 	r.HandleFunc("/contact", contact)
 	r.HandleFunc("/faq", faq)
 	http.ListenAndServe(":3000", r)
+}
+
+func must(err error) {
+	if err != nil {
+		panic(err)
+	}
 }
